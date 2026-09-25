ef91022908c1371d8769e075ee7c54f09af132ea
Nori by Desa <332777880+nori-by-desa[bot]@users.noreply.github.com>
2026-09-25 00:10:10 +0000

    Desa exact checkpoint

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,8 +33,8 @@ export const metadata: Metadata = {
   },
 };
 export const viewport: Viewport = {
-  themeColor: "#ffffff",
-  colorScheme: "light",
+  themeColor: "#091426",
+  colorScheme: "dark",
 };
 export default function Layout({
   children,
